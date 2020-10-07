0d972f9ece4254cd1bec0022681560e9f3f4f384
Nayara Ribeiro Freire <nayfreireof@gmail.com>
2020-10-07 20:18:27 -0300

    First commit

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet} from 'react-native';
+
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
+
+import Home from './src/pages/Home'
+import Login from './src/pages/Login'
+import RegisterUser from './src/pages/RegisterUser'
+// import RegisterSubject from './src/pages/crud/subjects/RegisterSubject'
+// import UpdateSubject from './src/pages/crud/subjects/UpdateSubject'
+// import DeleteSubject from './src/pages/crud/subjects/DeleteSubject'
+// import ViewSubject from './src/pages/crud/subjects/ViewSubject'
+// import ViewAllSubjects from './src/pages/crud/subjects/ViewAllSubjects'
+
+let Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="RegisterUser" component={RegisterUser} options={{title: "Cadastre-se"}}/> 
+        <Stack.Screen name="Login" component={Login} options={{title: "Login"}}/> 
+        <Stack.Screen name="Home" component={Home} options={{title: "Página Inicial"}}/> 
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {TextInput, Text, TouchbleOpacity, ScrollView, View} from 'react-native'
+
+export default function RegisterUser(){
+    return(
+        <ScrollView>
+            <View>
+                <Text>Bem-vindo</Text>
+                <Text>Dê início aos seus eestudos agora mesmo</Text>
+
+                <Text>Nome</Text>
+                <TextInput />
+
+                <Text>Email</Text>
+                <TextInput />
+
+                <Text>Senha</Text>
+                <TextInput />
+            </View>
+        </ScrollView>
+    )
+}
